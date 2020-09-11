ae5347c7ac340413a53e65c039856cf2f4a46c0f
unknown <abc123@gmail.com>
2020-09-11 12:51:06 +0530

    deploy my kepper app to github pages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import App from './components/App';
+
+
+function App(){
+  return (
+     <App />
+  )
+}
+export default App;
