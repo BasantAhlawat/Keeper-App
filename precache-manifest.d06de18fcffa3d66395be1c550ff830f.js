fe4c80162551403f444d39cd01f981cd85baa9ff
BasantAhlawat <basant.ahawalat2@gmail.com>
2020-09-11 14:18:33 +0530

    Updates

---
+++ precache-manifest.d06de18fcffa3d66395be1c550ff830f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26d6a6d191bbed4c02be655f56b42d6",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "e0600b4f291a33dbc73b",
+    "url": "/keeper-app/static/css/main.dc80348c.chunk.css"
+  },
+  {
+    "revision": "3f2666c5b1786a7f1fc4",
+    "url": "/keeper-app/static/js/2.14f8fc12.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper-app/static/js/2.14f8fc12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0600b4f291a33dbc73b",
+    "url": "/keeper-app/static/js/main.e3f1087b.chunk.js"
+  },
+  {
+    "revision": "3f7ec364bc84954cf321",
+    "url": "/keeper-app/static/js/runtime-main.afaa4161.js"
+  }
+]);
